443a34c003f88c7b8ad866e14d65459c880cb4fc
DoDoSunDoDo <76849863+DoDoSunDoDo@users.noreply.github.com>
2021-10-31 20:37:06 -0700

    Add files via upload

---
+++ BackEnd/src/java/HappyStay/repository/StayRepository.java
@@ -0,0 +1,15 @@
+package com.laioffer.staybooking.repository;
+import com.laioffer.staybooking.model.User;
+import java.util.List;
+
+import com.laioffer.staybooking.model.Stay;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StayRepository extends JpaRepository<Stay, Long> {
+    List<Stay> findByHost(User user);
+
+    List<Stay> findByIdInAndGuestNumberGreaterThanEqual(List<Long> ids, int guestNumber);
+}
+
